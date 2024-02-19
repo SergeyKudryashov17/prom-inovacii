cb88a053c6e58371c87f455bfbf23b7559beaeef
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-02-19 23:56:01 +0300

    Закончена страница О компании, сделана страница Контакты

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 document.addEventListener('scroll', (event) => {
     console.log(event);
-})
+});
+
+const dataItemTitles = document.querySelectorAll('.data-item__title');
+dataItemTitles.forEach(item => {
+    item?.addEventListener('click', () => {
+        const block = item.closest('.data-item');
+        block.classList.toggle('data-item_open');
+    });
+});
+
+const payItemTitles = document.querySelectorAll('.pay-item__title');
+payItemTitles.forEach(item => {
+    item?.addEventListener('click', () => {
+        const block = item.closest('.pay-item');
+        block.classList.toggle('pay-item_open');
+    });
+});
